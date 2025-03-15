c59ea92610fd7c2646da28285c11811c69f175a2
1lch2 <1lch2@163.com>
2025-03-15 18:44:56 +0800

    fix bug

--- generate_lora_tag.py
+++ generate_lora_tag.py
@@ -2,27 +2,29 @@ import os
 import re
 import pyperclip
 
-DIR = os.path.join(os.path.dirname(__file__), '../../output')
+DIR = os.path.join(os.path.dirname(__file__), "output")
+
 
 def compare():
     files = os.listdir(DIR)
-    match_reg = re.compile(r'^.+-\d+')
+    match_reg = re.compile(r"^.+-\d+")
 
     prompts = []
-    weight = '1'
+    weight = "1"
     lora_list = []
     for file in files:
         if not match_reg.match(file):
             continue
 
-        lora_prompt = '<lora:' + file.replace('.safetensors', f':{weight}>')
+        lora_prompt = "<lora:" + file.replace(".safetensors", f":{weight}>")
         lora_list.append(lora_prompt)
-    
-    lora_prompts = '\n'.join(lora_list)
+
+    lora_prompts = "\n".join(lora_list)
     prompts.append(lora_prompts)
     print(lora_prompts)
-    
-    pyperclip.copy('\n'.join(prompts))
+
+    pyperclip.copy("\n".join(prompts))
+
 
 if __name__ == "__main__":
     compare()
--- rename.py
+++ rename.py
@@ -1,24 +1,28 @@
 import os
 import re
 
-DIR = os.path.join(os.path.dirname(__file__), '../../output')
+DIR = os.path.join(os.path.dirname(__file__), "output")
+
 
 def rename():
     files = os.listdir(DIR)
-    if files[0] == '.keep':
+    if files[0] == ".gitkeep":
         files.pop(0)
 
-    regex = re.compile(r'\.safetensors$')
-    prefix = files[0].split('-')[0]
+    regex = re.compile(r"-\d+\.safetensors$")
+    prefix = files[0].split("-")[0]
 
     for i, file_name in enumerate(files):
         if not regex.search(file_name):
             continue
 
-        new_name = re.sub(f'^{prefix}', f'{prefix}-___', file_name).replace('___', f'{i}.safetensors')
+        new_name = re.sub(
+            r"-0\d+\.safetensors$", f"-{i}.safetensors", file_name, count=1
+        )
 
         print(new_name)
         os.rename(os.path.join(DIR, file_name), os.path.join(DIR, new_name))
+
 
 if __name__ == "__main__":
     rename()
